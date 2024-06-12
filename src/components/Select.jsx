f921a543032592530779441a615925136d068d6d
Swastik Kumar Majhi <46529877+corps21@users.noreply.github.com>
2024-06-12 12:45:45 +0000

    install html parser and added add post code

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
 
-function Select() {
+function Select({
+  options=[{name:"Active",value:"active"},{name:"Inactive",value:"inactive"}],
+  label="",
+  className = "",
+  containerClass = "",
+  ...props
+}) {
   return (
-    <div>
-      <select name="" id="">
-        <option value="">Active</option>
-        <option value="">Inactive</option>
+    <div className={`space-y-2 pt-[1rem] pb-[2rem] ${containerClass}`}>
+      {label && <label className="text-bold text-xl">{label}</label>}
+
+      <select
+        className={`w-full p-[.65rem] text-lg bg-gray-200 rounded-lg ${className}`}
+        {...props}
+      >
+        {options.map((option) => (<option key={option.name} value={option.value}>{option.name}</option>))}
+        
       </select>
     </div>
-  )
+  );
 }
 
-export default Select
+export default Select;
--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -1,35 +1,58 @@
-import { Input, Button, Container, Select } from "../components/index"
-import { useForm } from "react-hook-form"
+import { Input, Button, Container, Select } from "../components/index";
+import { useForm } from "react-hook-form";
 import { Editor } from "@tinymce/tinymce-react";
 import conf from "../conf/conf";
-
-// Select, Button, file input, 
+import { useRef, useState } from "react";
+import databaseService from "../appwrite/databaseService";
 
 function AddPost() {
-
   const { register, handleSubmit } = useForm();
+  const [file, setFile] = useState(null);
+  const imgRef = useRef();
+
+  const onSubmitHandler = ({
+    title,
+    slug,
+    content,
+    featuredImage,
+    status,
+    userId,
+  }) => {
+    setFile(imgRef.current.value);
+  };
 
   return (
     <Container className="border-2 rounded-lg p-5">
-      <form className="flex">
-
-        <div className="w-[50%] space-y-4 p-[2rem] py-[4rem] border-r-2">
-          <Input label="Title" {...register("title", {required:true})}/>
-          <Input label="Slug" />
-          <Editor apiKey={conf.tinymceKey} />
+      <form className="flex" handleSubmit={onSubmitHandler}>
+        <div className="w-[50%] p-[2rem] py-[4rem] border-r-2">
+          <Input label="Title" {...register("title", { required: true })} />
+          <Input
+            label="Slug"
+            containerClass="mt-[1rem]"
+            {...register("slug")}
+          />
+          <div className="h-[25rem] bg-gray-300 rounded-xl mt-[2.5rem]">
+            <Editor
+              apiKey={conf.tinymceKey}
+              initialValue="Welcome to BlogSphere!"
+              init={{
+                plugins:
+                  "anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage advtemplate ai mentions tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss markdown",
+                toolbar:
+                  "undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat",
+              }}
+            />
+          </div>
         </div>
 
-        <div className="w-[50%] py-[8rem] px-[8rem]">
-          <Input label="Featured Image" type="file" />
-          <Select />
-          <Button type="submit" text="Submit" className="w-full"/>
+        <div className="w-[50%] px-[8rem] py-[4rem]">
+          <Input label="Featured Image" type="file" className="" ref={imgRef} />
+          <Select label="Post Status" />
+          <Button type="submit" text="Submit" className="w-full" />
         </div>
-
-
-
       </form>
     </Container>
-  )
+  );
 }
 
-export default AddPost
+export default AddPost;
