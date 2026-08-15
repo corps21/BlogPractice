a215ade6cf25449abcb7c1aa478e708d01418752
Swastik Majhi <tatya-bicchu-21@proton.me>
2026-08-15 21:41:26 +0530

    feat: integrate search functionality by updating CustomSearchBox to handle search and suggestions, and refactor Search page to utilize these handlers

--- src/components/custom/CustomSearchBox.jsx
+++ src/components/custom/CustomSearchBox.jsx
@@ -1,47 +1,51 @@
 import { useState, useEffect } from "react";
 import useDebounce from "@/hooks/use-debounce";
-import { postService } from "@/service/postService";
 import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
-// TODO: integrate the tan stack query
-export function CustomSearchBox() {
+export function CustomSearchBox({onSearch, onSuggestions}) {
 	const [query, setQuery] = useState("");
-		const deboundedQuery = useDebounce(query, 500);
+		const debouncedQuery = useDebounce(query, 500);
 		const [suggestions, setSuggestions] = useState([]);
 		const [selectedSuggestion, setSelectedSuggestion] = useState(true);
 	
 		const onKeyDownHandler = async (e) => {
+			if(query.length === 0) {
+				return;
+			}
+
 			if (e.key === "Enter") {
 				if(selectedSuggestion || suggestions.length === 0 && query.length > 0) {
-					const res = await postService.searchPosts({ query });
-					console.log(res)
+					setSuggestions([])
+					setQuery("")
+					onSearch({query})
 				}
 			}
 		}
 	
 		useEffect(() => {
-			async function getItems() {
-				const res = (await postService.suggestSearchPost({ query: deboundedQuery })).data?.suggestions ?? []
-				setSuggestions(res)
-				console.log(res)
+			if(!debouncedQuery) {
+				setSuggestions([])
 			}
-			if (!selectedSuggestion) {
+
+			async function getItems() {
+				const res = await onSuggestions({query: debouncedQuery})
+				setSuggestions(res)
+			}
+
+			if (!selectedSuggestion && debouncedQuery) {
 				getItems()
 			}
-		}, [deboundedQuery, selectedSuggestion])
+		}, [debouncedQuery, selectedSuggestion, onSuggestions]);
 	
 		return (
-			<>
-				<Command className="pb-2">
+			<article className="px-6 max-w-4xl mx-auto">
+				<Command className="pb-2 h-auto">
 					<CommandInput value={query} onValueChange={setQuery} placeholder="Search..." onKeyDown={(e) => {
 						onKeyDownHandler(e);
+						setSuggestions([]);
 						setSelectedSuggestion(false);
 					}} />
-	
-					{query.length > 0 && suggestions.length === 0 && (
-						<CommandEmpty className="p-2 pt-4 ">No suggestions found</CommandEmpty>
-					)}
-	
+
 					{
 						suggestions.length > 0 && (
 							<CommandGroup heading="Suggestions">
@@ -61,6 +65,6 @@ export function CustomSearchBox() {
 						)
 					}
 				</Command>
-			</>
+			</article>
 		);
 }
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,28 @@
 import { CustomSearchBox } from "@/components/custom/CustomSearchBox";
+import { postService } from "@/service/postService";
+import {PostList} from "../components";
+import { useState } from "react";
 
+const onSuggestionsHandler = async (data) => {
+	const res = (await postService.suggestSearchPost(data)).data?.suggestions ?? []
+	console.log(res)
+	return res
+};
+
+// Add tanstack query;
 export default function Search() {
+	const [result, setResult] = useState([]);
+
+	const onSearchHandler = async (data) => {
+		const res = await postService.searchPosts(data);
+		setResult(res.data?.posts ?? [])
+		console.log(res)
+	};
 
 	return (
-		<>
-			<CustomSearchBox />
-		</>
+		<section className="pb-10">
+			<CustomSearchBox onSearch={onSearchHandler} onSuggestions={onSuggestionsHandler} />
+			<PostList isLoading={false} files={result} className="mt-6"/>
+		</section>
 	);
 }
